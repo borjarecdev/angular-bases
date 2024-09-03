e09a29dec8c106b7e33855dfa1f78a4acf61550c
Borja Recio <borjarec2@gmail.com>
2024-09-03 19:30:22 +0200

    Fin de sección 4

---
+++ src/app/counter/counter.module.ts
@@ -0,0 +1,28 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-counter',
+  template: `
+
+    <h3>Counter: {{ counter }}</h3>
+
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="increaseBy(-1)">-1</button>
+    <button (click)="resetBy(10)">Reset</button>
+  `
+})
+
+export class CounterModule {
+    public counter: number = 10;
+
+  increaseBy( value: number ): void {
+    this.counter += value;
+  }
+
+  resetBy( value: number ): void {
+    this.counter = value;
+  }
+
+
+}
+
